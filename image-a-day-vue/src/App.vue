4f07c99674db04d8c0fc98c83f27118212391c22
Vered Rekanati <veredrec@gmail.com>
2018-03-15 14:35:52 -0400

    Setup for Vue and webpack Relates #1

---
+++ image-a-day-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png"/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .logo {
+    width: 10rem;
+    height: 10rem;
+  }
+</style>
