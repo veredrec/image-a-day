5c925209f969fe48b7eeb34da2a74c734e76aea8
Vered Rekanati <veredrec@gmail.com>
2018-03-15 17:06:23 -0400

    Add favicon and add content and initial design to home page Relates #2 #3 #6

--- image-a-day-vue/src/router/index.js
+++ image-a-day-vue/src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
+import Home from './../components/Home';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
