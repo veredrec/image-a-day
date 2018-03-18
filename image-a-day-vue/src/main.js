b85940d88552fb2434f4c054172383d358284105
Vered Rekanati <veredrec@gmail.com>
2018-03-18 08:54:06 -0400

    Add footer component Relates #8

--- image-a-day-vue/src/main.js
+++ image-a-day-vue/src/main.js
@@ -2,19 +2,16 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 Vue.config.productionTip = false;
 
 Vue.component('app-header', Header);
+Vue.component('app-footer', Footer);
 
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>',
-  data() {
-    return {
-      items: ['1', '2', '3']
-    };
-  }
+  template: '<App/>'
 });
