4318201ec882e4df33347614fe4d3d306d9f9ca3
Vered Rekanati <veredrec@gmail.com>
2018-03-19 07:58:39 -0400

    Add alt tags to images from data Relates #18

--- image-a-day-vue/src/components/Home/Home.vue
+++ image-a-day-vue/src/components/Home/Home.vue
@@ -10,19 +10,23 @@
         images: [
         { source: "/march/bike.png",
           text: "Lime Bike in the Back Yard",
-          date: "March 15, 2018"
+          date: "March 15, 2018",
+          alt: "Bike leaning on a tree in the yard"
         },
         { source: "/march/tweet.png",
           text: "100 Days Of Code",
-          date: "March 16, 2018"
+          date: "March 16, 2018",
+          alt: "Tweet for joining the 100 days of code coding challenge"
         },
         { source: "/march/squirrel.jpg",
           text: "Squirrel From the Window",
-          date: "March 17, 2018"
+          date: "March 17, 2018",
+          alt: "A squirrel eats a nut in the front yard"
         },
         { source: "/march/lake.png",
           text: "Potato Creek State Park, IN",
-          date: "March 18, 2018"
+          date: "March 18, 2018",
+          alt: "A lake at a state park"
         }
         ]
       }
