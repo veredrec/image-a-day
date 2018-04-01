369ed128076266182ce616eefdde688044005d86
Vered Rekanati <veredrec@gmail.com>
2018-04-01 15:10:09 -0400

    Add font awesome to page with vue-awesome

--- image-a-day-vue/build/webpack.base.conf.js
+++ image-a-day-vue/build/webpack.base.conf.js
@@ -64,6 +64,12 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/vue-awesome')]
       }
     ]
   },
--- image-a-day-vue/src/components/Home/Home.vue
+++ image-a-day-vue/src/components/Home/Home.vue
@@ -6,47 +6,46 @@
 
 <script>
 import Firebase from 'firebase';
-  let config = {
-    apiKey: "AIzaSyDLQms4Q-ZUdADyWjCVOsWXzfTzsXxjiKo",
-    authDomain: "image-a-day.firebaseapp.com",
-    databaseURL: "https://image-a-day.firebaseio.com",
-    projectId: "image-a-day",
-    storageBucket: "image-a-day.appspot.com",
-    messagingSenderId: "20401690956"
-  }
+let config = {
+  apiKey: 'AIzaSyDLQms4Q-ZUdADyWjCVOsWXzfTzsXxjiKo',
+  authDomain: 'image-a-day.firebaseapp.com',
+  databaseURL: 'https://image-a-day.firebaseio.com',
+  projectId: 'image-a-day',
+  storageBucket: 'image-a-day.appspot.com',
+  messagingSenderId: '20401690956'
+};
 
-  let app = Firebase.initializeApp(config);
-  let db = app.database();
+let app = Firebase.initializeApp(config);
+let db = app.database();
 
-  let imagesRef = db.ref('images');
+let imagesRef = db.ref('images');
 
-  export default {
-    name: 'App',
-    firebase: {
-      images: imagesRef
-    },
-    data () {
-      return {
-        newImage: {
-          text: '',
-          source: '',
-          date: '',
-          alt: ''
-        }
+export default {
+  name: 'App',
+  firebase: {
+    images: imagesRef
+  },
+  data() {
+    return {
+      newImage: {
+        text: '',
+        source: '',
+        date: '',
+        alt: ''
       }
+    };
+  },
+  methods: {
+    addImage: function() {
+      imagesRef.push(this.newImage);
+      this.newImage.text = '';
+      this.newImage.source = '';
+      this.newImage.date = '';
+      this.newImage.alt = '';
     },
-    methods: {
-      addImage: function(){
-        imagesRef.push(this.newImage);
-        this.newImage.text = '';
-        this.newImage.source = '';
-        this.newImage.date = '';
-        this.newImage.alt = '';
-      },
-      removeImage: function(image){
-        imagesRef.child(image['.key']).remove();
-      }
+    removeImage: function(image) {
+      imagesRef.child(image['.key']).remove();
     }
   }
-
+};
 </script>
