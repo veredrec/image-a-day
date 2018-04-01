297dbdc0ad41530d16fdfbd21dc9dc51000289bb
Vered Rekanati <veredrec@gmail.com>
2018-04-01 16:52:54 -0400

    Add routes and menu links to more, about and gallery pages Relates #33

--- image-a-day-vue/src/router/index.js
+++ image-a-day-vue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './../components/Home/Home';
+import About from './../components/About/About';
+import More from './../components/More/More';
 
 Vue.use(Router);
 
@@ -11,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/more',
+      name: 'More',
+      component: More
     }
   ]
 });
